378d57b2a5ee91e3e5a923c043f0e53a16188c9c
ericmuchenah <31503581+ericmuchenah@users.noreply.github.com>
2019-12-05 19:08:17 +0300

    Add files via upload

---
+++ src/javafx/login/LoginController.java
@@ -0,0 +1,43 @@
+package javafx.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void loginAction(ActionEvent event) {
+        String username = usernameField.getText().trim();
+        String password = passwordField.getText();
+
+        if (username.isEmpty()) {
+            showAlertMessage(Alert.AlertType.ERROR, "Username Required!",
+                    "Please enter your username");
+            return;
+        }
+        if (password.isEmpty()) {
+            showAlertMessage(Alert.AlertType.ERROR, "Password Required!",
+                    "Please enter your password");
+            return;
+        }
+        showAlertMessage(Alert.AlertType.INFORMATION, "Details sent to database!",
+                "Username and password have been sent to database for validation");
+    }
+
+    public static void showAlertMessage(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+}
